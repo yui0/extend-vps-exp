e69e14a422d30391ab81b672acd4f81250602e55
GitHub30 <funaox@gmail.com>
2025-07-15 21:18:01 +0900

    Update main.mjs

--- main.mjs
+++ main.mjs
@@ -24,17 +24,18 @@ try {
         }
     }
 
-    await page.goto('https://secure.xserver.ne.jp/xapanel/login/xserver/', { waitUntil: 'networkidle2' })
+    await page.goto('https://secure.xserver.ne.jp/xapanel/login/xvps/', { waitUntil: 'networkidle2' })
     await page.locator('#memberid').fill(process.env.EMAIL)
     await page.locator('#user_password').fill(process.env.PASSWORD)
     await page.locator('text=ログインする').click()
     await page.waitForNavigation({ waitUntil: 'networkidle2' })
-    await page.goto('https://secure.xserver.ne.jp/xapanel/xvps/index', { waitUntil: 'networkidle2' })
+    await setTimeout(2000)
     await page.locator('.contract__menuIcon').click()
     await page.locator('text=契約情報').click()
     await page.locator('text=更新する').click()
     await page.locator('text=引き続き無料VPSの利用を継続する').click()
     await page.waitForNavigation({ waitUntil: 'networkidle2' })
+    await setTimeout(2000)
     await page.locator('text=無料VPSの利用を継続する').click()
 } catch (e) {
     console.error(e)
